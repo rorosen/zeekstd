0a4e4c4051f2c7a76a81c7fd97fcfdf3dcba2d38
Robert Rose <robert.rose@mailbox.org>
2025-03-18 23:49:39 +0100

    tests: reformat

--- tests/list.nix
+++ tests/list.nix
@@ -2,18 +2,14 @@
   zeekstd,
   runCommand,
 }:
-runCommand "zeekstd-list-test"
-  {
-    nativeBuildInputs = [ zeekstd ];
-  }
-  ''
-    set -x
+runCommand "zeekstd-list-test" { nativeBuildInputs = [ zeekstd ]; } ''
+  set -x
 
-    seq 20000 | zeekstd -o num.zst
+  seq 20000 | zeekstd -o num.zst
 
-    # Just check that list works, no proper test yet
-    zeekstd l num.zst
-    zeekstd l num.zst --from start --to end
+  # Just check that list works, no proper test yet
+  zeekstd l num.zst
+  zeekstd l num.zst --from start --to end
 
-    touch $out
-  ''
+  touch $out
+''
--- tests/partial.nix
+++ tests/partial.nix
@@ -2,28 +2,24 @@
   zeekstd,
   runCommand,
 }:
-runCommand "zeekstd-list-test"
-  {
-    nativeBuildInputs = [ zeekstd ];
-  }
-  ''
-    set -x
+runCommand "zeekstd-partial-test" { nativeBuildInputs = [ zeekstd ]; } ''
+  set -x
 
-    seq 20000 > num
-    zeekstd --max-frame-size 1K num -o num.zst
+  seq 20000 > num
+  zeekstd --max-frame-size 1K num -o num.zst
 
-    # Decompress first frame
-    zeekstd d -c --from-frame 0 --to-frame 0 num.zst > first
-    size_first=$(stat -c %s first)
-    [ $size_first -le 1024 ] || exit 1
-    head -c "$size_first" num | cmp first
+  # Decompress first frame
+  zeekstd d -c --from-frame 0 --to-frame 0 num.zst > first
+  size_first=$(stat -c %s first)
+  [ $size_first -le 1024 ] || exit 1
+  head -c "$size_first" num | cmp first
 
-    # Produces the same output independent of frame size
-    zeekstd --max-frame-size 8K num -o num.8k.zst
-    zeekstd d -c --from 12348 --to 12362 num.zst > partial
-    zeekstd d -c --from 12348 --to 12362 num.8k.zst > partial.8k
-    cmp partial partial.8k
-    echo -en "2692\n2693\n2694" | cmp partial
+  # Produces the same output independent of frame size
+  zeekstd --max-frame-size 8K num -o num.8k.zst
+  zeekstd d -c --from 12348 --to 12362 num.zst > partial
+  zeekstd d -c --from 12348 --to 12362 num.8k.zst > partial.8k
+  cmp partial partial.8k
+  echo -en "2692\n2693\n2694" | cmp partial
 
-    touch $out
-  ''
+  touch $out
+''
