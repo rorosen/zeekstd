434fca79c9f6c857ee4923461bdd28c9bc0aa315
rorosen <robert.rose@mailbox.org>
2025-12-20 16:35:35 +0100

    lib: run benches only with dickens
    Only use dickens.txt for benchmarks. Additionally, add comparison
    benchmarks of zstd-rs compression

--- lib/benches/compress.rs
+++ lib/benches/compress.rs
@@ -1,10 +1,9 @@
 use criterion::{Criterion, Throughput, criterion_group, criterion_main};
 use std::{hint::black_box, io::Write};
 use zeekstd::{EncodeOptions, Encoder, RawEncoder};
+use zstd::stream::raw::Operation;
 
 const DICKENS: &[u8] = include_bytes!("../../assets/dickens.txt");
-const OOFFICE: &[u8] = include_bytes!("../../assets/ooffice.exe");
-const OSDB: &[u8] = include_bytes!("../../assets/osdb.bin");
 
 fn raw_compress(enc: &mut RawEncoder, input: &[u8], output: &mut [u8]) {
     let mut in_prog = 0;
@@ -22,11 +21,6 @@ fn raw_compress(enc: &mut RawEncoder, input: &[u8], output: &mut [u8]) {
     }
 }
 
-fn compress(enc: &mut Encoder<'_, Vec<u8>>, input: &[u8]) {
-    enc.write_all(input).unwrap();
-    enc.end_frame().unwrap();
-}
-
 fn raw_compression(c: &mut Criterion) {
     let mut enc = EncodeOptions::new()
         .compression_level(1)
@@ -42,21 +36,12 @@ fn raw_compression(c: &mut Criterion) {
         });
     });
 
-    group.throughput(Throughput::Bytes(OOFFICE.len() as u64));
-    group.bench_function("ooffice", |b| {
-        b.iter(|| {
-            raw_compress(&mut enc, black_box(OOFFICE), black_box(&mut output));
-        });
-    });
-
-    group.throughput(Throughput::Bytes(OSDB.len() as u64));
-    group.bench_function("osdb", |b| {
-        b.iter(|| {
-            raw_compress(&mut enc, black_box(OSDB), black_box(&mut output));
-        });
-    });
-
     group.finish();
+}
+
+fn compress(enc: &mut Encoder<'_, Vec<u8>>, input: &[u8]) {
+    enc.write_all(input).unwrap();
+    enc.end_frame().unwrap();
 }
 
 fn compression(c: &mut Criterion) {
@@ -73,22 +58,40 @@ fn compression(c: &mut Criterion) {
         });
     });
 
-    group.throughput(Throughput::Bytes(OOFFICE.len() as u64));
-    group.bench_function("ooffice", |b| {
-        b.iter(|| {
-            compress(&mut enc, black_box(OOFFICE));
-        });
-    });
+    group.finish();
+}
 
-    group.throughput(Throughput::Bytes(OSDB.len() as u64));
-    group.bench_function("osdb", |b| {
+fn zstd_rs_compress(enc: &mut zstd::stream::raw::Encoder, input: &[u8], output: &mut [u8]) {
+    let mut in_prog = 0;
+
+    while in_prog != input.len() {
+        let stat = enc.run_on_buffers(&input[in_prog..], output).unwrap();
+        in_prog += stat.bytes_read;
+    }
+
+    loop {
+        let mut out_buffer = zstd_safe::OutBuffer::around(output);
+        let n = enc.finish(&mut out_buffer, false).unwrap();
+        if n == 0 {
+            break;
+        }
+    }
+}
+
+fn zstd_rs_compression(c: &mut Criterion) {
+    let mut enc = zstd::stream::raw::Encoder::new(1).unwrap();
+    let mut group = c.benchmark_group("zstd_rs_compression");
+    let mut output = vec![0; zstd_safe::CCtx::out_size()];
+
+    group.throughput(Throughput::Bytes(DICKENS.len() as u64));
+    group.bench_function("dickens", |b| {
         b.iter(|| {
-            compress(&mut enc, black_box(OSDB));
+            zstd_rs_compress(&mut enc, black_box(DICKENS), black_box(&mut output));
         });
     });
 
     group.finish();
 }
 
-criterion_group!(benches, raw_compression, compression);
+criterion_group!(benches, raw_compression, compression, zstd_rs_compression,);
 criterion_main!(benches);
--- lib/benches/decompress.rs
+++ lib/benches/decompress.rs
@@ -3,8 +3,16 @@ use std::{hint::black_box, io::Write};
 use zeekstd::{BytesWrapper, Decoder, Encoder};
 
 const DICKENS: &[u8] = include_bytes!("../../assets/dickens.txt");
-const OOFFICE: &[u8] = include_bytes!("../../assets/ooffice.exe");
-const OSDB: &[u8] = include_bytes!("../../assets/osdb.bin");
+
+fn dickens_compressed() -> Vec<u8> {
+    let mut buf = vec![];
+    let mut enc = Encoder::new(&mut buf).unwrap();
+
+    enc.write_all(DICKENS).unwrap();
+    enc.finish().unwrap();
+
+    buf
+}
 
 fn decompress(dec: &mut Decoder<'_, BytesWrapper<'_>>, buf: &mut [u8]) {
     loop {
@@ -16,50 +24,17 @@ fn decompress(dec: &mut Decoder<'_, BytesWrapper<'_>>, buf: &mut [u8]) {
 }
 
 fn decompression(c: &mut Criterion) {
-    let mut dickens = vec![];
-    let mut ooffice = vec![];
-    let mut osdb = vec![];
     let mut buf = vec![0; zstd_safe::DCtx::out_size()];
-    let mut enc_dickens = Encoder::new(&mut dickens).unwrap();
-    let mut enc_ooffice = Encoder::new(&mut ooffice).unwrap();
-    let mut enc_osdb = Encoder::new(&mut osdb).unwrap();
 
-    enc_dickens.write_all(DICKENS).unwrap();
-    let len_dickens = enc_dickens.finish().unwrap();
-    enc_ooffice.write_all(OOFFICE).unwrap();
-    let len_ooffice = enc_ooffice.finish().unwrap();
-    enc_osdb.write_all(OSDB).unwrap();
-    let len_osdb = enc_osdb.finish().unwrap();
-
-    let dickens = BytesWrapper::new(&dickens[..len_dickens as usize]);
-    let mut dec_dickens = Decoder::new(dickens).unwrap();
-    let ooffice = BytesWrapper::new(&ooffice[..len_ooffice as usize]);
-    let mut dec_ooffice = Decoder::new(ooffice).unwrap();
-    let osdb = BytesWrapper::new(&osdb[..len_osdb as usize]);
-    let mut dec_osdb = Decoder::new(osdb).unwrap();
+    let comp = dickens_compressed();
+    let mut dec = Decoder::new(BytesWrapper::new(&comp)).unwrap();
 
     let mut group = c.benchmark_group("decompression");
-    group.throughput(Throughput::Bytes(len_dickens));
+    group.throughput(Throughput::Bytes(DICKENS.len() as u64));
     group.bench_function("dickens", |b| {
         b.iter(|| {
-            decompress(black_box(&mut dec_dickens), black_box(&mut buf));
-            dec_dickens.reset();
-        });
-    });
-
-    group.throughput(Throughput::Bytes(len_ooffice));
-    group.bench_function("ooffice", |b| {
-        b.iter(|| {
-            decompress(black_box(&mut dec_ooffice), black_box(&mut buf));
-            dec_ooffice.reset();
-        });
-    });
-
-    group.throughput(Throughput::Bytes(len_osdb));
-    group.bench_function("osdb", |b| {
-        b.iter(|| {
-            decompress(black_box(&mut dec_osdb), black_box(&mut buf));
-            dec_osdb.reset();
+            decompress(&mut dec, black_box(&mut buf));
+            dec.reset();
         });
     });
 }
